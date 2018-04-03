875239e6bcba97ada079def6689640fe53b221ab
Zane <zane.franklin@gtri.gatech.edu>
2018-04-03 12:15:00 -0400

    download: get episode links

--- download.py
+++ download.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python3
+
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
+import re
 
 root = "http://www.officequotes.net/index.php"
 
 req = requests.get(root)
-r.raise_for_status()
+req.raise_for_status()
 
-soup = BeautifulSoup(r.content, "lxml")
+eps_link_re = re.compile("no\d-\d+.php")
+eps_links = filter(lambda link: hasattr(link, 'href'), BeautifulSoup(req.content, "lxml", parse_only=SoupStrainer('a', href=eps_link_re)))
 
-naveps = soup.find_all("div", {"class": "navEp"})[0]
+for link in eps_links:
+    print(link['href'])
+
+#naveps = soup.find_all("div", {"class": "navEp"})[0]
