5473490fefdd0ed4a15cff8c80bf80f4745adef1
zane <njineer@outlook.com>
2018-04-03 23:01:37 -0400

    quote extraction work

--- download.py
+++ download.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 import requests
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup, SoupStrainer, Doctype
 import re
 
 def getEpisodeQuotes(url):
     req = requests.get(url)
     req.raise_for_status()
     soup = BeautifulSoup(req.content, "lxml", parse_only=SoupStrainer("div", {"class": "quote"}))
+
+    # remove font setting tags
     tags_to_remove = ("b", "i", "u")
     for tag in tags_to_remove:
         for match in soup.findAll(tag):
             match.unwrap()
-    for i, quote in enumerate(soup):
-        print(quote)
-        if i > 3:
-            break
+
+    # remove all <br/> line break tags
+    for linebreak in soup.findAll("br"):
+        linebreak.extract()
+
+    for quote in filter(lambda q: type(q) is not Doctype, soup):
+        print(type(quote))
+        break
 
 
 
