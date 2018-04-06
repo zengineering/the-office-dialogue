953d36c124ce6906cbeb17a8c8aaa521ef8c898e
zane <njineer@outlook.com>
2018-04-06 13:34:32 -0400

    refactor in progress

--- download.py
+++ download.py
@@ -1,45 +1,25 @@
 #!/usr/bin/env python3
 
 import requests
-from bs4 import BeautifulSoup, SoupStrainer, Doctype
 import re
-from urllib.parse import urljoin, urlsplit
+from bs4 import BeautifulSoup, SoupStrainer, Doctype
+from urllib.parse import urljoin
 from urllib.error import URLError
 from collections import namedtuple
+from database import Database, OfficeQuote
+from queue import Queue
+from threading import Thread
+from sys import stderr
 
 Episode = namedtuple("Episode", ['number', 'season', 'scenes'])
 Quote = namedtuple("Quote", ['speaker', 'line'])
 Scene = namedtuple("Scene", ['quotes', 'deleted'])
 
-def parseScene(scene_text):
-    # split on newlines and remove empty items
-    scene = list(filter(lambda string: string and not string.isspace(), scene_text.split("\n")))
 
-    # Check if this is a deleted scene; first item is "Deleted Scene <index>"
-    deleted = scene[0].rsplit(None, 1)[0].lower() == "deleted scene"
-
-    if deleted:
-        scene = scene[1:]
-
-    line_pairs = map(lambda p: Quote(p[0].strip(), p[1].strip()), (line.split(":", 1) for line in scene))
-
-    return Scene(line_pairs, deleted)
-
-
-def parseEpisodePage(url):
-    match = re.fullmatch("\/no(\d)-(\d+).php", urlsplit(url).path)
-    if match:
-        season, episode = match.groups()
-    else:
-        raise URLError("Unexpected URL format: {}".format(url))
-
-    # request the page
-    req = requests.get(url)
-    req.raise_for_status()
-
+def parseEpisodePage(content):
     # parse only <div class="quote"> blocks
     # NOTE: SoupStrainer does not remove Doctype; filter later
-    soup = BeautifulSoup(req.content, "lxml", parse_only=SoupStrainer("div", {"class": "quote"}))
+    soup = BeautifulSoup(content, "lxml", parse_only=SoupStrainer("div", {"class": "quote"}))
 
     # remove font setting (<b>, <i>, <u>) tags
     tags_to_remove = ("b", "i", "u")
@@ -56,10 +36,25 @@ def parseEpisodePage(url):
         spacer.decompose()
 
     # remove Doctype if nessessary; extract text from each quote block
-    scene_texts = [quote_div.text for quote_div in filter(lambda q: type(q) is not Doctype, soup)]
+    scene_texts = [quote_div.text for quote_div in filter(lambda q: not isinstance(q, Doctype), soup)]
 
     # return [ season, episode, [(speaker, spoken)], <deleted scene?> ]
-    return Episode(episode, season, (parseScene(st) for st in scene_texts))
+    return (parseScene(st) for st in scene_texts)
+
+
+def parseScene(scene_text):
+    # split on newlines and remove empty items
+    scene = list(filter(lambda string: string and not string.isspace(), scene_text.split("\n")))
+
+    # Check if this is a deleted scene; first item is "Deleted Scene <index>"
+    deleted = scene[0].rsplit(None, 1)[0].lower() == "deleted scene"
+
+    if deleted:
+        scene = scene[1:]
+
+    line_pairs = map(lambda p: Quote(p[0].strip(), p[1].strip()), (line.split(":", 1) for line in scene))
+
+    return Scene(line_pairs, deleted)
 
 
 def writeToDatabase(episode, db):
@@ -74,23 +69,44 @@ def writeToDatabase(episode, db):
                 deleted=deleted))
 
 
-def main():
-    root = "http://www.officequotes.net/index.php"
-    req = requests.get(root)
+def fetchPage(url):
+    req = requests.get(url)
     req.raise_for_status()
+    return req.content
 
-    eps_link_re = re.compile("no\d-\d+.php")
-    eps_links = filter(lambda link: hasattr(link, "href"), BeautifulSoup(req.content, "lxml", parse_only=SoupStrainer("a", href=eps_link_re)))
 
+def getEpisodeUrls(index_url):
+    index_content = fetchPage(index_url)
+
+    # extract links to episode pages
+    eps_link_re = re.compile("no(\d)-(\d+).php")
+    return filter(lambda link: hasattr(link, "href"),
+        BeautifulSoup(index_content, "lxml", parse_only=SoupStrainer("a", href=eps_link_re)))
+
+
+def main():
+    root_url = "http://www.officequotes.net/index.php"
+    eps_urls = getEpisodeUrls(root_url)
+    print (eps_urls)
+    return
+
+    episodes = []
     try:
         #episodes = (parseEpisodePage(urljoin(root, link["href"])) for link in eps_links)
-        for link in eps_links:
-            url = urljoin(root, link["href"])
-            print(url)
-            parseEpisodePage(url)
+        for eps_url in map(lambda a: a["href"], eps_urls):
+            match = re.fullmatch(eps_link_re, eps_url["href"])
+            if match:
+                season, episode = match.groups()
+            else:
+                raise URLError("Unexpected URL format: {}".format(eps_url["href"]), stderr)
+                continue
+
+            url = urljoin(root_url, eps_url["href"])
+            scenes = parseEpisodePage(fetchPage(url))
+            episodes.append(episode, season, scenes)
             break
+
     except (URLError, requests.http.HTTPError) as e:
         print("Parsing ({}) failed with error: {}".format(url, e))
-
 
 main()
