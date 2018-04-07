6e317934dd448b602b9a621687fdf4f296f3be92
zane <njineer@outlook.com>
2018-04-06 23:16:52 -0400

    container cleanup

--- containers.py
+++ containers.py
@@ -1,5 +1,4 @@
 from collections.abc import Iterable
-
 class Episode():
     def __init__(self, number, season, scenes):
         if not isinstance(number, int):
@@ -14,10 +13,24 @@ class Episode():
         self.scenes = list(scenes)
 
     def __repr__(self):
-        return "<Episode(number={}, season={}, scenes={})".format(self.number, self.seasons, self.scenes)
+        return "<Episode(number={}, season={}, scenes={})>".format(self.number, self.seasons, self.scenes)
 
     def __str__(self):
         return "S{}E{}: {} scenes".format(self.season, self.number, len(self.scenes))
+
+
+class Scene():
+    def __init__(self, quotes, deleted):
+        if not isinstance(quotes, Iterable):
+            raise ValueError("Episode arg1 expects an Iterable; got a {}".format(type(quotes)))
+        if not isinstance(deleted, bool):
+            raise ValueError("Episode arg2 expects a bool; got a {}".format(type(deleted)))
+
+        self.quotes = list(quotes)
+        self.deleted = deleted
+
+    def __repr__(self):
+        return "<Scene(quotes={}, deleted={})>".format(self.quotes, self.deleted)
 
 
 class Quote():
@@ -32,18 +45,4 @@ class Quote():
 
     def __repr__(self):
         return "<Quote(speaker={}, line={})>".format(self.speaker, self.line)
-
-
-class Scene():
-    def __init__(self, quotes, deleted):
-        if not isinstance(quotes, Iterable):
-            raise ValueError("Episode arg1 expects an Iterable; got a {}".format(type(quotes)))
-        if not isinstance(deleted, bool):
-            raise ValueError("Episode arg2 expects a bool; got a {}".format(type(deleted)))
-
-        self.quotes = quotes
-        self.deleted = deleted
-
-    def __repr__(self):
-        return "<Scene(quotes={}, deleted={})".format(self.quotes, self.deleted)
 
