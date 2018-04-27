8d97373aba678039f68e8e3c78bf82cfaaf305a3
Zane <zane.franklin@gtri.gatech.edu>
2018-04-27 18:06:53 -0400

    take an arg

--- analysis/src/main/kotlin/com/github/zengineering/TheOffice.kt
+++ analysis/src/main/kotlin/com/github/zengineering/TheOffice.kt
@@ -1,5 +1,24 @@
 package com.github.zengineering
 
+
+// Argparser
+fun help(): Unit = println(
+        "usage: ./TheOfficeKt <db_path>\n"
+)
+
 fun main(args: Array<String>) {
+    if (args.isNotEmpty()) {
+        when (args[0]) {
+            "-h", "--help" -> help()
+            else -> { 
+                analyze(args[0]); 
+            }
+        }
+    } else {
+        help()
+    }
+}
+
+fun analyze(dbPath: String) {
     println("Hello, Kotlin")
 }
