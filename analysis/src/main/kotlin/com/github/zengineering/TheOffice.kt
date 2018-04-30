3a12e8e1c2bedd8e145446d30ce015c2d0cdc65f
Zane <zane.franklin@gtri.gatech.edu>
2018-04-30 16:09:56 -0400

    move to Exposed with xerial's sqlite-jdbc

--- analysis/src/main/kotlin/com/github/zengineering/TheOffice.kt
+++ analysis/src/main/kotlin/com/github/zengineering/TheOffice.kt
@@ -1,10 +1,37 @@
 package com.github.zengineering
 
+import java.sql.DriverManager
+import java.sql.Connection.TRANSACTION_SERIALIZABLE
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.transactions.ThreadLocalTransactionManager
+
+//object OfficeQuotes: IntIdTable("office_quotes") {
+object OfficeQuotes: Table("office_quotes") {
+    val id = integer("id").autoIncrement().primaryKey()
+    val season = integer("season")
+    val episode = integer("episode")
+    val scene = integer("episode")
+    val speaker = text("speaker")
+    val line = text("line")
+    val deleted = bool("deleted")
+}
+
+fun analyze(dbPath: String) {
+    Database.connect(
+        { DriverManager.getConnection("jdbc:sqlite:$dbPath") }, 
+        { ThreadLocalTransactionManager(it, TRANSACTION_SERIALIZABLE) }
+    )
+
+    transaction {
+        OfficeQuotes.select { OfficeQuotes.id eq 1 }.singleOrNull()?.let {
+            println(it[OfficeQuotes.line])
+        }?: println("nothing")
+    }
+}
 
 // Argparser
-fun help(): Unit = println(
-        "usage: ./TheOfficeKt <db_path>\n"
-)
+fun help(): Unit = println("usage: ./TheOfficeKt <db_path>\n")
 
 fun main(args: Array<String>) {
     if (args.isNotEmpty()) {
@@ -19,6 +46,3 @@ fun main(args: Array<String>) {
     }
 }
 
-fun analyze(dbPath: String) {
-    println("Hello, Kotlin")
-}
