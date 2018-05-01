0594db3bd687722c6d11a15fa77d2f6e94c1cbb3
Zane <zane.franklin@gtri.gatech.edu>
2018-05-01 17:40:44 -0400

    limit speaker frequency

--- sql-analysis/commands.sql
+++ sql-analysis/commands.sql
@@ -1,1 +1,1 @@
-SELECT speaker, count(*) as frequency FROM office_quotes GROUP BY speaker ORDER BY count(*) DESC;
+SELECT speaker, count(*) as frequency FROM office_quotes GROUP BY speaker ORDER BY count(*) DESC LIMIT 30;
