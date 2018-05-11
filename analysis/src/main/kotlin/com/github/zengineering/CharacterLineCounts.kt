7a294310878c80a314f5f1aa138017a8695194ac
zane <njineer@outlook.com>
2018-05-10 22:42:46 -0400

    reorg line counts by character instead of season

--- analysis/src/main/kotlin/com/github/zengineering/CharacterLineCounts.kt
+++ analysis/src/main/kotlin/com/github/zengineering/CharacterLineCounts.kt
@@ -5,24 +5,23 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.File
 import com.google.gson.Gson
 
-fun countLinesPerCharacter(dbPath: String) {
-    var characterLineCounts = mapOf<Int, Map<String, Int>>()
+fun countSeasonalLines(dbPath: String) {
+    var seasonalLineCounts = mutableMapOf<String, MutableMap<Int, Int>>()
     connectDatabase(dbPath)
     transaction {
-        characterLineCounts = (1..9).associateBy(
-            { it },
-            { season -> OfficeQuotes
-                .slice(OfficeQuotes.speaker, OfficeQuotes.speaker.count())
-                .select { OfficeQuotes.season eq season }
-                .groupBy(OfficeQuotes.speaker)
-                .associateBy(
-                    { it[OfficeQuotes.speaker] },
-                    { it[OfficeQuotes.speaker.count()] }
-                )
-            }
-        )
+        (1..9).forEach { season -> OfficeQuotes
+            .slice(OfficeQuotes.speaker, OfficeQuotes.speaker.count())
+            .select { OfficeQuotes.season eq season }
+            .groupBy(OfficeQuotes.speaker)
+            .forEach {
+                seasonalLineCounts.getOrPut(it[OfficeQuotes.speaker]) { mutableMapOf<Int, Int>() }
+                    .put(season, it[OfficeQuotes.speaker.count()])
+             }
+        }
     }
-    Gson().run { File("characterLineCount.json").writeText(this.toJson(characterLineCounts)) }
+    Gson().run { File("seasonalLineCount.json").writeText(this.toJson(
+        seasonalLineCounts.filter { (_, seasons) -> seasons.values.sum() > 100 }
+    )) }
 }
 
 
@@ -30,6 +29,6 @@ fun main(args: Array<String>) {
     if (args.isEmpty() or args.contains("-h") or args.contains("--help")) {
         println("usage: ./CharacterLineCountsKt <db_path>\n")
     } else { 
-        countLinesPerCharacter(args[0])
+        countSeasonalLines(args[0])
     }
 }
