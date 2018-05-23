e9f0c67ca4e861966945212ca67bd0dd2678cb45
Zane <zane.franklin@gtri.gatech.edu>
2018-05-23 09:52:21 -0500

    error handling for invalid db path in line counts

--- analysis/src/main/kotlin/com/github/zengineering/CharacterLineCounts.kt
+++ analysis/src/main/kotlin/com/github/zengineering/CharacterLineCounts.kt
@@ -4,24 +4,29 @@ import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.File
 import com.google.gson.Gson
+import java.io.FileNotFoundException
 
 fun countSeasonalLines(dbPath: String) {
     var seasonalLineCounts = mutableMapOf<String, MutableMap<Int, Int>>()
-    connectDatabase(dbPath)
-    transaction {
-        (1..9).forEach { season -> OfficeQuotes
-            .slice(OfficeQuotes.speaker, OfficeQuotes.speaker.count())
-            .select { OfficeQuotes.season eq season }
-            .groupBy(OfficeQuotes.speaker)
-            .forEach {
-                seasonalLineCounts.getOrPut(it[OfficeQuotes.speaker]) { mutableMapOf<Int, Int>() }
-                    .put(season, it[OfficeQuotes.speaker.count()])
-             }
+    try {
+        connectDatabase(dbPath)
+        transaction {
+            (1..9).forEach { season -> OfficeQuotes
+                .slice(OfficeQuotes.speaker, OfficeQuotes.speaker.count())
+                .select { OfficeQuotes.season eq season }
+                .groupBy(OfficeQuotes.speaker)
+                .forEach {
+                    seasonalLineCounts.getOrPut(it[OfficeQuotes.speaker]) { mutableMapOf<Int, Int>() }
+                        .put(season, it[OfficeQuotes.speaker.count()])
+                 }
+            }
         }
+        Gson().run { File("seasonalLineCount.json").writeText(this.toJson(
+            seasonalLineCounts.filter { (_, seasons) -> seasons.values.sum() > 100 }
+        )) }
+    } catch (e: FileNotFoundException) {
+        System.err.println("Database not found at $dbPath") 
     }
-    Gson().run { File("seasonalLineCount.json").writeText(this.toJson(
-        seasonalLineCounts.filter { (_, seasons) -> seasons.values.sum() > 100 }
-    )) }
 }
 
 
