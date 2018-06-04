3e12ea209563980492bf5c9899e178bb19d922a6
zane <njineer@outlook.com>
2018-06-03 22:51:41 -0400

    modularize for other plots

--- visualization/plot.py
+++ visualization/plot.py
@@ -53,28 +53,25 @@ def characterDataToArrays(character_data):
 def main(json_path):
     character_data = sorted(parseData(json_path), key=lambda char: char.total_lines, reverse=False)
     names, lines, episodes = characterDataToArrays(character_data)
-    plotTotalLines(names, lines)
+    plotTotalLines(names, lines, xticks=np.arange(0, 13001, 1000))
 
-def plotTotalLines(names, lines, *, bar_height=0.8, colors=None, dpi=200, tick_font_size=6):
+
+def plotTotalLines(names, lines, *, outfile="the-office-lines.png", xticks=None, bar_height=0.8, colors=None, dpi=200, font_size=6):
     num_seasons = lines.shape[1]-1
     y_pos = np.arange(len(names))
     seasons = np.arange(1, num_seasons+1)
     if not colors:
-        #colors = ("", "brown", "limegreen", "blue", "yellow", "aqua", "darkorange", "black", "red", "purple")
         colors = ("", "brown", "yellow", "blue", "limegreen", "darkorange", "aqua", "purple", "red", "black")
 
     plots = [mplot.barh(y_pos, lines[:, s], bar_height, lines[:, 0:s].sum(1), color=colors[s]) for s in seasons]
 
     mplot.title("Number of dialogue lines per character")
-    mplot.xticks(np.arange(0, 13001, 1000), fontsize=tick_font_size)
-    mplot.yticks(y_pos, names, fontsize=tick_font_size)
-    mplot.legend((p[0] for p in plots), ("Season {}".format(s) for s in seasons))
-    print("axes")
-
+    mplot.yticks(y_pos, names, fontsize=font_size)
+    if xticks is not None:
+        mplot.xticks(xticks, fontsize=font_size)
+    mplot.legend((p[0] for p in plots), ("Season {}".format(s) for s in seasons), fontsize=font_size)
     mplot.savefig("the-office-lines.png", orientation="landscape", dpi=200)
-    print("saved")
     mplot.close()
-    print("closed")
 
 
 if __name__ == "__main__":
