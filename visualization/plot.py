3744b8c440fb179c137080eb612d001b73f40381
Zane <zane.franklin@gtri.gatech.edu>
2018-06-04 14:36:06 -0400

    add lines-per-episode

--- visualization/plot.py
+++ visualization/plot.py
@@ -6,10 +6,22 @@ import numpy as np
 from argparse import ArgumentParser
 
 class Character():
+    '''
+    Data class to store character dialogue data
+    '''
     def __init__(self, name, lines, episodes):
+        '''
+        name: name of the character
+        lines: [] of line counts for each season
+        episodes: [] of line counts for each season
+
+        lines and episodes are len=10 (9 seasons plus 0-index)
+        '''
         self.name = name
         self.lines = lines
         self.episodes = episodes
+        self.lines_per_episode = [self.lines[i]/self.episodes[i] if self.episodes[i] != 0 else 0
+            for i in range(len(self.episodes))]
 
     @property
     def total_lines(self):
@@ -19,63 +31,88 @@ class Character():
     def total_episodes(self):
         return sum(self.episodes)
 
+    @property
+    def total_lines_per_episode(self):
+        return sum(self.lines_per_episode)
+
+
 
 def parseArgs():
+    '''
+    Commandline argument parsing
+    '''
     ap = ArgumentParser(description = "Plot The Office character dialogue stats.")
     ap.add_argument('input_file', help="JSON with character dialogue info")
     return ap.parse_args()
 
 
 def parseData(json_path):
+    '''
+    Read JSON file of the format
+        { character: { season: { episode: line count } } }
+    Parse into and return a list of Characters
+    '''
     with open(json_path) as json_fp:
         dialogue = json.load(json_fp)
 
     characters = []
 
     for speaker, seasons in dialogue.items():
-        lineCounts = [0] * 10
-        episodeCounts = [0] * 10
+        line_counts = [0] * 10
+        episode_counts = [0] * 10
         for season, episodes in seasons.items():
-            lineCounts[int(season)] = sum(map(int, episodes.values()))
-            episodeCounts[int(season)] = len(episodes)
-        characters.append(Character(speaker, lineCounts, episodeCounts))
+            line_counts[int(season)] = sum(map(int, episodes.values()))
+            episode_counts[int(season)] = len(episodes)
+        characters.append(Character(speaker, line_counts, episode_counts))
 
     return characters
 
 
-def characterDataToArrays(character_data):
-    names = np.array([char.name for char in character_data])
-    lines = np.array([char.lines for char in character_data])
-    episodes = np.array([char.episodes for char in character_data])
-    return names, lines, episodes
-
-
-def main(json_path):
-    character_data = parseData(json_path)
-
-    names, lines, episodes = characterDataToArrays(sorted(character_data, key=lambda char: char.total_lines))
-    plotByCharacter(names, lines, "the-office-lines.png", title="Number of dialogue lines per character", xticks=np.arange(0, 13001, 1000))
-
-    names, lines, episodes = characterDataToArrays(sorted(character_data, key=lambda char: char.total_episodes))
-    plotByCharacter(names, episodes, "the-office-episodes.png", title="Number of dialogue episodes per character")
-
-
-def plotByCharacter(names, lines, outfile, *, title=None, xticks=None, bar_height=0.8, colors=None, dpi=200, font_size=6):
-    num_seasons = lines.shape[1]-1
+def plotByCharacter(names, data, outfile, *, title=None, xticks=None, bar_height=0.8, colors=None, dpi=200, font_size=6):
+    '''
+    Plot a horizontal-stacked-bar chart of character info
+    '''
+    num_seasons = data.shape[1]-1
     y_pos = np.arange(len(names))
     seasons = np.arange(1, num_seasons+1)
     if not colors:
         colors = ("", "brown", "yellow", "blue", "limegreen", "darkorange", "aqua", "purple", "red", "black")
 
-    plots = [mplot.barh(y_pos, lines[:, s], bar_height, lines[:, 0:s].sum(1), color=colors[s]) for s in seasons]
+    plots = [mplot.barh(y_pos, data[:, s], bar_height, data[:, 0:s].sum(1), color=colors[s]) for s in seasons]
 
     mplot.title(title)
     mplot.yticks(y_pos, names, fontsize=font_size)
     if xticks is not None:
         mplot.xticks(xticks, fontsize=font_size)
     mplot.legend((p[0] for p in plots), ("Season {}".format(s) for s in seasons), fontsize=font_size)
-    mplot.savefig(outfile, orientation="landscape", dpi=200)
+    mplot.savefig(outfile, orientation="landscape", dpi=dpi)
     mplot.close()
+
+
+def main(json_path):
+    '''
+    Parse JSON and plot it.
+    TODO: DRY this out.
+    '''
+    character_data = parseData(json_path)
+
+    # dialogue lines
+    character_data.sort(key=lambda char: char.total_lines)
+    names = np.array([char.name for char in character_data])
+    lines = np.array([char.lines for char in character_data])
+    plotByCharacter(names, lines, "the-office-lines.png", title="Dialogue lines per character", xticks=np.arange(0, 13001, 1000))
+
+    # episodes
+    character_data.sort(key=lambda char: char.total_episodes)
+    names = np.array([char.name for char in character_data])
+    episodes = np.array([char.episodes for char in character_data])
+    plotByCharacter(names, episodes, "the-office-episodes.png", title="Episodes (with dialogue) per character")
+
+    # lines per episode
+    character_data.sort(key=lambda char: char.total_lines_per_episode)
+    names = np.array([char.name for char in character_data])
+    lines_per_episode = np.array([char.lines_per_episode for char in character_data])
+    plotByCharacter(names, lines_per_episode, "the-office-lines-per-episode.png", title="Dialogue lines per episode per character")
 
 
 if __name__ == "__main__":
