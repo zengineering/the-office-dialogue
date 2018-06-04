c4cc57bdef4160a7acec95c0e040e1d8ccb77758
zane <njineer@outlook.com>
2018-06-03 22:43:05 -0400

    total line count

--- visualization/plot.py
+++ visualization/plot.py
@@ -43,30 +43,32 @@ def parseData(json_path):
     return characters
 
 
-def main(json_path):
-    characters = parseData(json_path)
-    characters.sort(key=lambda char: char.total_lines, reverse=True)
-    characters = characters[:5]
-    #print(characters)
+def characterDataToArrays(character_data):
+    names = np.array([char.name for char in character_data])
+    lines = np.array([char.lines for char in character_data])
+    episodes = np.array([char.episodes for char in character_data])
+    return names, lines, episodes
 
-    num_seasons = 9
-    bar_height = 0.8
+
+def main(json_path):
+    character_data = sorted(parseData(json_path), key=lambda char: char.total_lines, reverse=False)
+    names, lines, episodes = characterDataToArrays(character_data)
+    plotTotalLines(names, lines)
+
+def plotTotalLines(names, lines, *, bar_height=0.8, colors=None, dpi=200, tick_font_size=6):
+    num_seasons = lines.shape[1]-1
+    y_pos = np.arange(len(names))
     seasons = np.arange(1, num_seasons+1)
-    y_pos = np.arange(len(characters))
-    plots = []
-    for i, char in enumerate(characters):
-        print("[{}] {}:".format(i, char.name))
-        for season in seasons:
-            plots.append(mplot.barh(y_pos, char.lines[season], bar_height, char.lines[season-1]))
-            print(season, end="...")
-        print()
+    if not colors:
+        #colors = ("", "brown", "limegreen", "blue", "yellow", "aqua", "darkorange", "black", "red", "purple")
+        colors = ("", "brown", "yellow", "blue", "limegreen", "darkorange", "aqua", "purple", "red", "black")
+
+    plots = [mplot.barh(y_pos, lines[:, s], bar_height, lines[:, 0:s].sum(1), color=colors[s]) for s in seasons]
 
     mplot.title("Number of dialogue lines per character")
-    mplot.ylabel("Character")
-    mplot.yticks(y_pos, map(lambda char: char.name, characters))
-    mplot.xlabel("Lines of dialogue")
-    #mplot.xticks(np.arange(0,70001, 500))
-    mplot.legend(map(lambda p: p[0], plots), map(lambda s: "Season {}".format(s), seasons))
+    mplot.xticks(np.arange(0, 13001, 1000), fontsize=tick_font_size)
+    mplot.yticks(y_pos, names, fontsize=tick_font_size)
+    mplot.legend((p[0] for p in plots), ("Season {}".format(s) for s in seasons))
     print("axes")
 
     mplot.savefig("the-office-lines.png", orientation="landscape", dpi=200)
