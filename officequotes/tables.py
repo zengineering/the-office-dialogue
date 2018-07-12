b610641dd83a5e5de13ec18dfc68ccaa0e39389a
zane <zane@vagrant.arch>
2018-07-12 17:52:24 -0400

    repr db tables

--- officequotes/tables.py
+++ officequotes/tables.py
@@ -8,16 +8,24 @@ Base = declarative_base()
 
 class Character(Base):
     __tablename__ = "characters"
+
     id = Column(Integer, primary_key=True)
     name = Column(Text, nullable=False)
     quote = relationship("OfficeQuote", back_populates="speaker")
 
+    def __repr__(self):
+        return "<Character(name={})>".format(self.name)
+
 
 class DialogueLine(Base):
     __tablename__ = "lines"
+
     id = Column(Integer, primary_key=True)
     content = Column(Text, nullable=False)
     quote = relationship("OfficeQuote", uselist=False, back_populates="line")
+
+    def __repr__(self):
+        return "<DialogueLine(content={})>".format(self.content)
 
 
 class OfficeQuote(Base):
@@ -27,11 +35,11 @@ class OfficeQuote(Base):
     season = Column(Integer)
     episode = Column(Integer)
     speaker_id = Column(Integer, ForeignKey('characters.id'))
-    speaker = relationship('Character', uselist=False, back_populates='quote')
     line_id = Column(Integer, ForeignKey('lines.id'))
-    line = relationship('DialogueLine', uselist=False, back_populates='quote')
     deleted = Column(Boolean)
 
+    speaker = relationship('Character', uselist=False, back_populates='quote')
+    line = relationship('DialogueLine', uselist=False, back_populates='quote')
 
     def __repr__(self):
         return "<OfficeQuote(season={}, episode={}, speaker_id={}, line_id={}, deleted={})>".format(
