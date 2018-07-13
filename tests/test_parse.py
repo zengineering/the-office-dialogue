94ffc86e4f59f672e0813cbc2b0f910daf6745ed
zane <zane@vagrant.arch>
2018-07-13 14:33:32 -0400

    move parse fixtures into parse test

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,11 +1,53 @@
 import pytest
-from bs4 import BeautifulSoup, Doctype
+from bs4 import BeautifulSoup, Doctype, SoupStrainer
 from itertools import tee
+from pathlib import Path
 
-from context import parse, dataclasses
+from context import test_path, parse, dataclasses
 from parse import withoutDoctypes, removeTags, parseScene, parseEpisode, extractMatchingUrls
 from dataclasses import Quote
 from download import eps_href_re
+
+@pytest.fixture
+def testSoup():
+    html = (
+        '<!DOCTYPE html> <html> '
+        '<head> <title>Page Title</title> </head> '
+        '<body>'
+        '<h1>Heading</h1>'
+        '<p>Paragraph</p>'
+        '<div class="spacer">&nbsp</div>'
+        '<b>Bold</b>'
+        '<i>Italics</i>'
+        '<u>Underline</u>'
+        '<b><i><u>All three</b></i></u>'
+        'Some text ending with a linebreak <br />'
+        '</body>'
+        '</html>'
+    )
+    return BeautifulSoup(html, "lxml")
+
+
+@pytest.fixture
+def episodeSoup():
+    html = episodeHtml()
+    soup = BeautifulSoup(html, "lxml", parse_only=SoupStrainer("div", {"class": "quote"}))
+    parse.removeTags(soup)
+    return soup
+
+
+@pytest.fixture
+def episodeHtml():
+    with open(Path(test_path)/'data/no5-13.php', 'rb') as f:
+        content = f.read()
+    return content
+
+
+@pytest.fixture
+def indexHtml():
+    with open(Path(test_path)/'data/index.php', 'rb') as f:
+        content = f.read()
+    return content
 
 
 def test_withoutDoctypes(testSoup):
@@ -58,4 +100,5 @@ def test_extractMatchingUrls(indexHtml):
     urls = list(extractMatchingUrls(indexHtml, eps_href_re))
     assert len(urls) == 186
     assert "no1-01.php" in urls
+    assert "no5-13.php" in urls
     assert "no9-23.php" in urls
