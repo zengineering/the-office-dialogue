54020e7e72d65866b2782be8e1603a2695c950fb
zane <zane@vagrant.arch>
2018-07-16 18:02:14 -0400

    db tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,6 +1,6 @@
 import pytest
 from tempfile import NamedTemporaryFile
-from sqlalchemy import exists
+from sqlalchemy import exists, func
 
 from context import database, dataclasses
 from dataclasses import Quote, Episode
@@ -19,18 +19,21 @@ def test_db_addQuote(db):
     addQuote(season, episode, deleted, speaker, line)
 
     with contextSession() as session:
-        char_q = session.query(Character).filter(Character.name==speaker).all()
-        line_q = session.query(DialogueLine).filter(DialogueLine.content==line).all()
-        quote_q = session.query(OfficeQuote).filter(
-            OfficeQuote.season==season, OfficeQuote.episode==episode).all()
+        assert session.query(Character).filter(Character.name==speaker).count() == 1
+        assert session.query(DialogueLine).filter(DialogueLine.content==line).count() == 1
+        assert session.query(OfficeQuote).filter(
+            OfficeQuote.season==season, OfficeQuote.episode==episode).count() == 1
 
-        assert len(char_q) == 1
-        assert len(line_q) == 1
-        assert len(quote_q) == 1
-        assert quote_q[0].speaker.id == char_q[0].id
-        assert quote_q[0].speaker.name == char_q[0].name
-        assert quote_q[0].line.id == line_q[0].id
-        assert quote_q[0].deleted == deleted
+        char = session.query(Character).filter(Character.name==speaker).first()
+        quote = session.query(OfficeQuote).filter(
+            OfficeQuote.season==season, OfficeQuote.episode==episode).first()
+        line = session.query(DialogueLine).filter(DialogueLine.content==line).first()
+
+
+        assert quote.speaker.id == char.id
+        assert quote.speaker.name == char.name
+        assert quote.line.id == line.id
+        assert quote.deleted == deleted
 
 
 def test_db_existingSpeaker(db):
@@ -47,18 +50,17 @@ def test_db_existingSpeaker(db):
             assert char_id == char_q[0].id
 
 
-#def test_db_addMultiple(db):
-#    season, episode, deleted, speaker, line = 5, 13, False, "Speaker", "Line content"
-#    for i in range(2):
-#        addQuote(season+i, episode+i, deleted, speaker, line)
-#
-#    with contextSession() as session:
-#        char_q = session.query(Character).filter(Character.name==speaker).all()
-#        assert len(char_q) == 1
-#        assert len(char_q[0].quotes) == 2
-#        line_q = session.query(DialogueLine).filter(DialogueLine.content==line).all()
-#        assert len(line_q) == 2
-#
-#        quote_q = session.query(OfficeQuote).all()
-#        assert len(quote_q) == 2
-#
+def test_db_addMultiple(db):
+    season, episode, deleted, speaker, line = 5, 13, False, "Speaker{}", "Line {}"
+    count = 1024
+    for i in range(count):
+        addQuote(season, episode, deleted, speaker.format(i%100), line.format(i))
+
+    with contextSession() as session:
+        assert session.query(Character.id).count() == 100
+        assert len(list(session.query(Character.name).distinct())) == 100
+        assert session.query(DialogueLine.id).count() == count
+
+        assert all(map(lambda s: s[0] == season, session.query(OfficeQuote.season).all()))
+        assert all(map(lambda e: e[0] == episode, session.query(OfficeQuote.episode).all()))
+
