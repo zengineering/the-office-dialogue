1d9576cc4cfd1020be36c275efab19230c53fd4a
zane <zane@vagrant.arch>
2018-07-16 18:16:17 -0400

    add quote fixture

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,9 +1,11 @@
 import pytest
 from tempfile import NamedTemporaryFile
 from sqlalchemy import exists, func
+from collections import namedtuple
+
+Quote = namedtuple("Quote", ('season', 'episode', 'deleted', 'speaker', 'line'))
 
 from context import database, dataclasses
-from dataclasses import Quote, Episode
 from database import *
 
 
@@ -14,53 +16,54 @@ def db(scope='module'):
         yield
 
 
-def test_db_addQuote(db):
-    season, episode, deleted, speaker, line = 1, 1, False, "Speaker", "Line content"
-    addQuote(season, episode, deleted, speaker, line)
+@pytest.fixture
+def quote():
+    return Quote(1, 1, False, "Speaker{}", "Line {}")
+
+
+def test_db_addQuote(db, quote):
+    addQuote(*quote)
 
     with contextSession() as session:
-        assert session.query(Character).filter(Character.name==speaker).count() == 1
-        assert session.query(DialogueLine).filter(DialogueLine.content==line).count() == 1
+        assert session.query(Character).filter(Character.name==quote.speaker).count() == 1
+        assert session.query(DialogueLine).filter(
+            DialogueLine.content==quote.line).count() == 1
         assert session.query(OfficeQuote).filter(
-            OfficeQuote.season==season, OfficeQuote.episode==episode).count() == 1
+            OfficeQuote.season==quote.season, OfficeQuote.episode==quote.episode
+            ).count() == 1
 
-        char = session.query(Character).filter(Character.name==speaker).first()
-        quote = session.query(OfficeQuote).filter(
-            OfficeQuote.season==season, OfficeQuote.episode==episode).first()
-        line = session.query(DialogueLine).filter(DialogueLine.content==line).first()
+        char = session.query(Character).filter(Character.name==quote.speaker).first()
+        officequote = session.query(OfficeQuote).filter(
+            OfficeQuote.season==quote.season, OfficeQuote.episode==quote.episode).first()
+        line = session.query(DialogueLine).filter(DialogueLine.content==quote.line).first()
+
+        assert officequote.speaker.id == char.id
+        assert officequote.speaker.name == char.name
+        assert officequote.line.id == line.id
+        assert officequote.deleted == quote.deleted
 
 
-        assert quote.speaker.id == char.id
-        assert quote.speaker.name == char.name
-        assert quote.line.id == line.id
-        assert quote.deleted == deleted
-
-
-def test_db_existingSpeaker(db):
-    season, episode, deleted, speaker, line = 1, 1, False, "Speaker", "Line content"
+def test_db_existingSpeaker(db, quote):
     for i in range(2):
-        addQuote(season+i, episode+i, deleted, speaker, line)
+        addQuote(quote.season+i, quote.episode+i, quote.deleted, quote.speaker, quote.line)
 
     with contextSession() as session:
-        char_q = session.query(Character).filter(Character.name==speaker).all()
-        assert len(char_q) == 1
-        assert len(char_q[0].quotes) == 2
-
-        for char_id, in session.query(OfficeQuote.speaker_id).all():
-            assert char_id == char_q[0].id
+        assert session.query(Character).filter(Character.name==quote.speaker).count() == 1
+        char = session.query(Character).filter(Character.name==quote.speaker).first()
+        assert session.query(OfficeQuote).filter(OfficeQuote.speaker_id==char.id).count() == 2
 
 
-def test_db_addMultiple(db):
-    season, episode, deleted, speaker, line = 5, 13, False, "Speaker{}", "Line {}"
+def test_db_addMultiple(db, quote):
     count = 1024
     for i in range(count):
-        addQuote(season, episode, deleted, speaker.format(i%100), line.format(i))
+        addQuote(quote.season, quote.episode, quote.deleted,
+                 quote.speaker.format(i%100), quote.line.format(i))
 
     with contextSession() as session:
         assert session.query(Character.id).count() == 100
         assert len(list(session.query(Character.name).distinct())) == 100
         assert session.query(DialogueLine.id).count() == count
 
-        assert all(map(lambda s: s[0] == season, session.query(OfficeQuote.season).all()))
-        assert all(map(lambda e: e[0] == episode, session.query(OfficeQuote.episode).all()))
+        assert all(map(lambda s: s[0] == quote.season, session.query(OfficeQuote.season).all()))
+        assert all(map(lambda e: e[0] == quote.episode, session.query(OfficeQuote.episode).all()))
 
