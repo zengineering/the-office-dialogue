8dc181bd73bfbeb8adba90b8fb4aa39062519c4a
zane <zane@vagrant.arch>
2018-07-19 14:17:34 -0400

    download app tests

--- tests/test_download_app.py
+++ tests/test_download_app.py
@@ -1,9 +1,11 @@
 import pytest
 from threading import current_thread
 from time import sleep
+from queue import Queue
 
 from context import download
-from download.threaded import writeEpisodeToDb, StoppingThread
+from download.download import eps_href_re
+from download.threaded import writeEpisodeToDb, StoppingThread, fetchAndParse
 from download.dataclasses import Episode, Quote
 from database import contextSession, Character, DialogueLine, OfficeQuote
 
@@ -37,5 +39,28 @@ def test_dlapp_stoppingThread():
     t.stop()
     t.join()
     assert 1
+
+
+def test_dlapp_fetchAndParse():
+    url_q = Queue()
+    eps_q = Queue()
+    fail_q = Queue()
+    index_url = "http://officequotes.net"
+    for url in ("5-13", "6-04"):
+        url_q.put("no{}.php".format(url))
+
+    # producer threads for fetching and parsing episode pages
+    t = StoppingThread(target=fetchAndParse, args=(url_q, eps_q, fail_q, eps_href_re, index_url))
+    t.start()
+    t.join()
+    assert url_q.empty()
+    assert fail_q.empty()
+    assert eps_q.qsize() == 2
+    while not eps_q.empty():
+        episode = eps_q.get()
+        assert isinstance(episode, Episode)
+        assert episode.season in (5,6)
+        assert episode.number in (4,13)
+        assert len(episode.quotes) > 100
 
 
