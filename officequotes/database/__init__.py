584eaa7250b7fe9368855eeb63049f5abaa68451
zane <njineer@outlook.com>
2018-07-22 15:49:07 -0400

    drop unneeded function

--- officequotes/database/__init__.py
+++ officequotes/database/__init__.py
@@ -1,3 +1,3 @@
 from .session_interface import setupDbEngine, contextSession, engineConnection
-from .db_utils import addQuote, makeQuote, getCharacter
+from .db_utils import addQuote, getCharacter
 from .tables import OfficeQuote, Character, DialogueLine
