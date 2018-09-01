b9433cb3e1de6ddb239772b677e94351fe111f32
zane <njineer@outlook.com>
2018-08-31 22:59:34 -0400

    async tests

--- tests/test_async.py
+++ tests/test_async.py
@@ -2,8 +2,10 @@ import asyncio
 import aiohttp
 import pytest
 
-from officequotes.download.app import fetch_content
-from officequotes.download.constants import req_headers
+from context import test_path
+from officequotes.download.app import fetch_content, fetch_and_parse
+from officequotes.download.constants import req_headers, eps_href_re
+from officequotes.download.dataclasses import Episode
 
 @pytest.fixture
 async def aioh_session():
@@ -16,8 +18,25 @@ def episode_url():
     return "http://officequotes.net/no5-13.php"
 
 
+def episodeHtml():
+    '''
+    Content of http://officequotes.net/no5-13.php
+    '''
+    with open(Path(test_path)/'data/no5-13.php', 'rb') as f:
+        content = f.read()
+    return content
+
+
 @pytest.mark.asyncio
 async def test_async_fetch_content(episode_url, aioh_session):
     content = await fetch_content(episode_url, aioh_session)
     assert b"These muffins taste bad" in content
 
+
+@pytest.mark.asyncio
+async def test_async_fetch_and_parse(aioh_session, episode_url):
+    episode = await fetch_and_parse(episode_url, eps_href_re, aioh_session)
+    assert isinstance(episode, Episode)
+    assert episode.season == 5
+    assert episode.number == 13
+    assert len(episode.quotes) > 100
