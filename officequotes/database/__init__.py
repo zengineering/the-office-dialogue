2e5befec8f1fbd4f8ead721db20dbf4ce99e1e62
zane <zane@vagrant.arch>
2018-09-07 18:24:37 -0400

    fix import

--- officequotes/database/__init__.py
+++ officequotes/database/__init__.py
@@ -1,3 +1,3 @@
 from .tables import OfficeQuote, Character, DialogueLine
 from .db_interface import setupDb, contextSession
-from .create_db import UniqueValueDict, addEpisodeToDb, addCharactersToDb
+from .create_db import create_db, UniqueValueDict, addEpisodeToDb, addCharactersToDb
